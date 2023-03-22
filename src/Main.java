f2eee2d7beea9cb30748ed781fa5b05a9f9af027
paulo <henriquepaulo0606@gmail.com>
2023-03-22 12:17:35 -0300

    Exercicio alura testando ponto flutuante

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Atribuindo valor com ponto flutuante
+        double salario = 1250.70;
+
+        // Testando o valor
+        System.out.println(" Meu salario é " +salario);
+
+        //  Variavel do tipo inteiro so volta resultado inteiro independente do calculo
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+
+        // Fazendo nova tentativa do tipo double sem casa decimais
+        double novaTentativa = 5 / 2;
+        System.out.println(novaTentativa);
+
+        // Fazendo nova tentativa com numeros com casa decimais que resulta o valor certo com ponto flutuante
+        double terceiraTentativa = 5.0 / 2.0;
+        System.out.println(terceiraTentativa);
+
+    }
+}
